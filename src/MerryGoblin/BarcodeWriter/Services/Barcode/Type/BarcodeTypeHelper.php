8c2ae2a17eb0fdf48c2233e1622ff5ffad3f4134
Keller Alexandre <keller.alexandre0@gmail.com>
2024-01-18 18:04:00 +0100

    main: Adding constant to provide a safe way to use type and being compliant with the way barcode generator does it too

--- src/MerryGoblin/BarcodeWriter/Services/Barcode/Type/BarcodeTypeHelper.php
+++ src/MerryGoblin/BarcodeWriter/Services/Barcode/Type/BarcodeTypeHelper.php
@@ -4,6 +4,49 @@ namespace MerryGoblin\BarcodeWriter\Services\Barcode\Type;
 
 class BarcodeTypeHelper
 {
+	const TYPE_UPC_A = 'UPCA';
+	const TYPE_UPC_E = 'UPCE';
+	const TYPE_EAN_2 = 'EAN2'; // Not handled
+	const TYPE_EAN_5 = 'EAN5'; // Not handled
+	const TYPE_EAN_8 = 'EAN8';
+	const TYPE_EAN_13 = 'EAN13';
+	const TYPE_EAN_13_NO_PAD = 'EAN13NOPAD';
+	const TYPE_EAN_13_PAD = 'EAN13PAD';
+	const TYPE_EAN_128 = 'EAN128';
+	const TYPE_EAN_128_A = 'EAN128A';
+	const TYPE_EAN_128_B = 'EAN128B';
+	const TYPE_EAN_128_C = 'EAN128C';
+	const TYPE_EAN_128_AC = 'EAN128AC';
+	const TYPE_EAN_128_BC = 'EAN128BC';
+	const TYPE_CODE_11 = 'CODE11'; // Not handled
+	const TYPE_CODE_39 = 'C39';
+	const TYPE_CODE_39_ASCII = 'C39ASCII';
+	const TYPE_CODE_39_CHECKSUM = 'C39+'; // Not handled
+	const TYPE_CODE_39E = 'C39E'; // Not handled
+	const TYPE_CODE_39E_CHECKSUM = 'C39E+'; // Not handled
+	const TYPE_CODE_93 = 'C93';
+	const TYPE_CODE_93_ASCII = 'C93ASCII';
+	const TYPE_CODE_128 = 'C128';
+	const TYPE_CODE_128_A = 'C128A';
+	const TYPE_CODE_128_B = 'C128B';
+	const TYPE_CODE_128_C = 'C128C';
+	const TYPE_CODE_128_AC = 'C128AC';
+	const TYPE_CODE_128_BC = 'C128BC';
+	const TYPE_STANDARD_2_5 = 'S25'; // Not handled
+	const TYPE_STANDARD_2_5_CHECKSUM = 'S25+'; // Not handled
+	const TYPE_INTERLEAVED_2_5 = 'I25'; // Not handled
+	const TYPE_INTERLEAVED_2_5_CHECKSUM = 'I25+'; // Not handled
+	const TYPE_MSI = 'MSI'; // Not handled
+	const TYPE_MSI_CHECKSUM = 'MSI+'; // Not handled
+	const TYPE_POSTNET = 'POSTNET'; // Not handled
+	const TYPE_PLANET = 'PLANET'; // Not handled
+	const TYPE_RMS4CC = 'RMS4CC'; // Not handled
+	const TYPE_KIX = 'KIX'; // Not handled
+	const TYPE_IMB = 'IMB'; // Not handled
+	const TYPE_CODABAR = 'CODABAR'; // Not handled
+	const TYPE_PHARMA_CODE = 'PHARMA'; // Not handled
+	const TYPE_PHARMA_CODE_TWO_TRACKS = 'PHARMA2T'; // Not handled
+
 	/**
 	 * @param string $type
 	 * @return BarcodeTypeInterface
@@ -32,27 +75,43 @@ class BarcodeTypeHelper
 			case 'ean13pad':
 				$barcodeType = new EAN13PadType();
 				break;
+			case 'c39':
 			case 'code39':
 				$barcodeType = new Code39Type();
 				break;
+			case 'c39ascii':
 			case 'code39ascii':
 				$barcodeType = new Code39AsciiType();
 				break;
+			case 'c93':
+			case 'code93':
+				$barcodeType = new Code93Type();
+				break;
+			case 'c93ascii':
+			case 'code93ascii':
+				$barcodeType = new Code93AsciiType();
+				break;
+			case 'c128':
 			case 'code128':
 				$barcodeType = new Code128Type();
 				break;
+			case 'c128a':
 			case 'code128a':
 				$barcodeType = new Code128AType();
 				break;
+			case 'c128b':
 			case 'code128b':
 				$barcodeType = new Code128BType();
 				break;
+			case 'c128c':
 			case 'code128c':
 				$barcodeType = new Code128CType();
 				break;
+			case 'c128ac':
 			case 'code128ac':
 				$barcodeType = new Code128ACType();
 				break;
+			case 'c128bc':
 			case 'code128bc':
 				$barcodeType = new Code128BCType();
 				break;
